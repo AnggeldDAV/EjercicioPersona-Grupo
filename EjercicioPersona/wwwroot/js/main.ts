c9b3b5d5fd845d0cab503883a052857568dd2b93
alvarezleiva <galvarezleiv@gmail.com>
2024-04-09 14:17:53 +0200

    html German

--- EjercicioPersona/wwwroot/js/main.ts
+++ EjercicioPersona/wwwroot/js/main.ts
@@ -130,6 +130,26 @@ class HTMLChurrutero implements ILibreriaHTML {
         return `<input type='number' id='${id}' placeholder='${placeholder}' />`;
     }
 }
+class HTMLGerman implements ILibreriaHTML {
+    public dameDiv(id: string): string {
+        return (`<div id='${id}'></div><br/>`);
+    }
+    public dameCss(): string {
+        return `<link href='/css/german.css' rel='stylesheet'>`;
+    }
+    public dameBoton(id: string, texto: string): string {
+        return `<input type='button' id=${id} value='${texto}' />`;
+    }
+    public dameCheckBox(id: string, contenido: string): string {
+        return `<input type='checkbox' id='${id}'/><label for='${id}'> ${contenido} </label>`;
+    }
+    public dameTextBox(id: string, placeholder: string): string {
+        return `<input type ='text' id='${id}' placeholder='${placeholder}'/>`;
+    }
+    public dameNumberBox(id: string, placeholder: string): string {
+        return `<input type='number' id='${id}' placeholder='${placeholder}' />`;
+    }
+}
 class HTMLBootStrap implements ILibreriaHTML {
     public dameDiv(id: string): string {
         return (`<div id='${id}' class='form-group col-md-6'></div><br/>`);
